8dbe13b93b880689adb0f8991f9755b39a2949d9
psylo-dev <davenplay@gmail.com>
2024-11-09 16:12:33 +0100

    Update logger.py

--- tornado/core/logger.py
+++ tornado/core/logger.py
@@ -1,46 +1,44 @@
-# logging outputs
-
 from datetime import datetime
 from importlib.metadata import version
 from colorama import Fore as clr, Style
 
+# Time and Date
 now = datetime.now()
 time_now = now.strftime("%d-%m-%Y %H:%M:%S")
 
+# Colors
 cyan = clr.CYAN
-green = '\033[92m'
-yellow = '\033[93m'
+green = '\033[92m'  # Hellgrün
+yellow = '\033[93m'  # Hellgelb
 purple = '\033[35m'
-red = clr.RED
+red = clr.RED  # Hellrot
 bright = Style.BRIGHT
 blue = clr.BLUE
 reset = Style.RESET_ALL
+grey = '\033[37m'  # Hellgrau
 Datetime = ("[" + cyan + time_now + reset + "]")
 
+# Header with updated details (Program name, version, build date)
 banner = f"""
-______      U  ___ u    ____       _   _         _        ____       U  ___ u 
-|_ " _|      \/"_ \/ U |  _"\ u   | \ |"|    U  /"\  u   |  _"\       \/"_ \/ 
-  | |        | | | |  \| |_) |/  <|  \| |>    \/ _ \/   /| | | |      | | | | 
- /| |\   .-,_| |_| |   |  _ <    U| |\  |u    / ___ \   U| |_| |\ .-,_| |_| | 
-u |_|U    \_)-\___/    |_| \_\    |_| \_|    /_/   \_\   |____/ u  \_)-\___/  
-_// \\_        \\      //   \\_   ||   \\,-.  \\    >>    |||_          \\    
-(__) (__)      (__)    (__)  (__)  (_")  (_/  (__)  (__)  (__)_)        (__)   v{version('tornado')}
-
-{green}\x1B[3manonymously reverse shell over tor network using hidden services without portforwarding\x1B[0m{reset}           
-                                {reset}\x1B[3msamet-g / samogod\x1B[0m{reset} 
+{grey}=============================================
+  PROGRAM NAME: Tornado Engine
+  VERSION: {version('tornado')}
+  BUILD DATE: {time_now}
+============================================={reset}
 """
 
+# Logging functions
 def good(msg):
     print("[" + green + bright + 'SUCCESS' + reset + '] ' + msg)
 
 def goodt(msg):
-    print(Datetime +" [" + green + bright +'SUCCESS' + reset + '] '+msg)
+    print(Datetime + " [" + green + bright + 'SUCCESS' + reset + '] ' + msg)
 
 def error(msg):
     print("[" + red + bright + 'ERR' + reset + '] ', msg)
 
 def errort(msg):
-    print(Datetime +" [" + red + bright +'ERR' + reset + '] ',msg)
+    print(Datetime + " [" + red + bright + 'ERR' + reset + '] ', msg)
 
 def info(msg):
     print("[" + blue + 'INF' + reset + "] " + msg)
@@ -49,10 +47,13 @@ def input(msg):
     print("[" + purple + 'INPUT' + reset + "] " + msg)
 
 def infot(msg):
-    print(Datetime +" [" + blue + 'INF' + reset + "] "+msg)
+    print(Datetime + " [" + blue + 'INF' + reset + "] " + msg)
 
 def warn(msg):
     print("[" + yellow + 'WRN' + reset + "] " + msg)
 
 def warnt(msg):
-    print(Datetime +" [" + yellow + 'WRN' + reset + "] "+msg)
+    print(Datetime + " [" + yellow + 'WRN' + reset + "] " + msg)
+
+# Display banner
+print(banner)
